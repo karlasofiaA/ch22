21531e2f49677199aab94cc45011d663be812204
Karla Arenas <karlasofia.arenasv@gmail.com>
2023-01-13 16:01:02 -0600

    feat: renderización de js03

--- js/js04/js04.js
+++ js/js04/js04.js
@@ -1,1 +1,17 @@
 console.log("Sesión JS04");
+
+const nombresCh22 = ['Manuel', 'Leonardo', 'Melani'];
+//Usando el constructor de la clase Array
+const nombresCh14 = new Array('Abue', 'Cori', 'Maga', 'Sebas');
+console.log(nombresCh22);
+console.log(nombresCh14);
+
+const promedioEdadesCh22 = [25];
+//Esta línea va a generar un array de 24 elementos
+const promedioEdadesCh14 = new Array(24);
+console.log(promedioEdadesCh22);
+console.log(promedioEdadesCh14);
+
+//Conocer la longitud de un arreglo
+console.log(`Número de elementos ${promedioEdadesCh22.length}`);
+console.log(`Número de elementos ${promedioEdadesCh14.length}`);
